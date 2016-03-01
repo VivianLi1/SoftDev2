526a9a58152af615696458b22bb0399c6874b14a
Vivian Li <vivian2938@gmail.com>
2016-03-01 11:47:19 -0500

    wtf

--- HW03/animate.js
+++ HW03/animate.js
@@ -3,12 +3,7 @@ var ctx = c.getContext("2d");
 
 var width = c.width;
 var height = c.height;
-<<<<<<< HEAD
 
-var radius = 10;
-var growing = true;
-
-=======
 var requestId;
 
 var radius = 0;
@@ -19,12 +14,13 @@ dvd.src = "logo_dvd.jpg";
 var dvd_h = 100;
 var dvd_w = 75;
 
+var requestId;
+
 var x = 0;
 var y = 0;
 var x_v = 2;
 var y_v = 2;
 
->>>>>>> dab6f71cfa9496d2994ad763aa94d0f4df39e5b9
 var draw = function(){
     if(radius >= width/2){
 	growing = false;
@@ -33,15 +29,9 @@ var draw = function(){
     }
 
     if(growing){
-<<<<<<< HEAD
-	radius = radius + 1;
-    }else{
-	radius = radius - 1;
-=======
 	radius++;
     }else{
 	radius--;
->>>>>>> dab6f71cfa9496d2994ad763aa94d0f4df39e5b9
     }
 
     ctx.clearRect(0,0,width,height);
@@ -51,19 +41,6 @@ var draw = function(){
     ctx.fillStyle = "red";
     ctx.fill();
 
-<<<<<<< HEAD
-    var requestID = window.requestAnimationFrame(draw);
-};
-
-var x = 0;
-var y = 0;
-var forward = true;
-
-var bounce = function(){
-
-    if(forward){
-	x = x + 1;
-=======
     requestId = window.requestAnimationFrame(draw);
 };
 
@@ -80,24 +57,10 @@ var bounce = function(){
 	x_v = x_v * -1;
     } else if (y >= height - dvd_w || y <= 0){
 	y_v = y_v * -1;
->>>>>>> dab6f71cfa9496d2994ad763aa94d0f4df39e5b9
     }
 
     ctx.clearRect(0,0,width,height);
     ctx.beginPath();
-<<<<<<< HEAD
-    ctx.arc(x, y, radius, 0, 2*Math.PI);
-    ctx.closePath();
-    ctx.fillStyle = "red";
-    ctx.fill();
-};
-
-var button = document.getElementById("go");
-button.addEventListener("click", bounce);
-
-var button2 = document.getElementById("stop");
-button.addEventListener("click", stop);
-=======
     ctx.drawImage(dvd, x, y, dvd_h, dvd_w);
     ctx.closePath();
 
@@ -110,4 +73,3 @@ var button3 = document.getElementById("bounce");
 button.addEventListener("click", draw);
 button2.addEventListener("click", stop);
 button3.addEventListener("click", bounce);
->>>>>>> dab6f71cfa9496d2994ad763aa94d0f4df39e5b9
--- HW04/svg.js
+++ HW04/svg.js
@@ -1,23 +1,66 @@
 var pic = document.getElementById("vimage");
 
+var width = pic.width;
+var height = pic.height;
+
+var requestId;
+
+var radius = 0;
+var growing = true;
+
+var dvd = new Image();
+dvd.src = "logo_dvd.jpg";
+var dvd_h = 100;
+var dvd_w = 75;
+
+var requestId;
+
+var x = 0;
+var y = 0;
+var x_v = 2;
+var y_v = 2;
+
+/*
 var change = function(e) {
     e.preventDefault();
     this.setAttribute("fill", "green");
 };
+*/
 
-var drawDot = function(x,y){
+var drawDot = function() {
 
-var c = document.createElementNS(
-    "http://www.w3.org/2000/svg", "circle");
-c.setAttribute( "cx", 0 );
-c.setAttribute( "cy", 0 );
-c.setAttribute( "r", "100" );
-c.setAttribute( "fill", "red" );
-c.setAttribute( "stroke", "black" );
+    if(radius >= width/2 || radius >= height/2){
+	growing = false;
+    } else if(radius <= 0){
+	growing = true;
+    }
 
-pic.appendChild( c );
+    if(growing){
+	radius++;
+    }else{
+	radius--;
+    }
+
+    
+    var c = document.createElementNS(
+	"http://www.w3.org/2000/svg", "circle");
+    c.setAttribute( "cx", 250);
+    c.setAttribute( "cy", 250 );
+    c.setAttribute( "r", radius );
+    c.setAttribute( "fill", "red" );
+    c.setAttribute( "stroke", "black" );
+
+    pic.appendChild( c );
+
+    requestId = window.requestAnimationFrame(drawDot);
+
 };
 
+var stop = function(){
+    window.cancelAnimationFrame(requestId);
+};
+
+/*
 var clicked = function(e){
     if(e.toElement == this){
 	drawDot(e.offsetX, e.offsetY);
@@ -25,3 +68,37 @@ var clicked = function(e){
 };
 
 pic.addEventListener("click", clicked);
+*/
+
+var bounce = function(){
+
+    x = x + x_v;
+    y = y + y_v;
+
+    if(x >= width - dvd_h || x <= 0){
+	x_v = x_v * -1;
+    } else if (y >= height - dvd_w || y <= 0){
+	y_v = y_v * -1;
+    }
+    
+     var c = document.createElementNS(
+	 "http://www.w3.org/2000/svg", "image");
+    c.setAttribute("xlink", "logo_dvd.jpg");
+    c.setAttribute("x", x);
+    c.setAttribute("y", y);
+    c.setAttribute("height", 75);
+    c.setAttribute("width", 100);
+
+    pic.appendChild(c);
+
+    requestId = window.requestAnimationFrame(bounce);
+
+};
+
+
+var button = document.getElementById("grow");
+var button2 = document.getElementById("stop");
+var button3 = document.getElementById("bounce");
+button.addEventListener("click", drawDot);
+button2.addEventListener("click", stop);
+button3.addEventListener("click", bounce);
