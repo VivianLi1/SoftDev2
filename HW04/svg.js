099657f090a81edee0c200e2462c7bbc1f255116
Vivian Li <vivian2938@gmail.com>
2016-03-04 11:44:49 -0500

    dvd

--- HW04/svg.js
+++ HW04/svg.js
@@ -5,25 +5,15 @@ var height = pic.height;
 
 var intervalId;
 
-var dvd = new Image();
-dvd.src = "logo_dvd.jpg";
-var dvd_h = 100;
-var dvd_w = 75;
-
-var x = 0;
-var y = 0;
-var x_v = 2;
-var y_v = 2;
+var c = document.createElementNS(
+    "http://www.w3.org/2000/svg", "circle");
+c.setAttribute("cx", 250);
+c.setAttribute("cy", 250);
+c.setAttribute("r", 0);
+c.setAttribute("fill", "red");
+c.setAttribute("stroke", "black");
 
 var drawDot = function() {
-
-    var c = document.createElementNS(
-	"http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", 250);
-    c.setAttribute("cy", 250);
-    c.setAttribute("r", 0);
-    c.setAttribute("fill", "red");
-    c.setAttribute("stroke", "black");
 
     var growing = true;
     
@@ -55,28 +45,46 @@ var stop = function(){
     window.clearInterval(intervalId);
 };
 
+
+var c = document.createElementNS(
+	    "http://www.w3.org/2000/svg", "image");
+c.setAttributeNS("http://www.w3.org/1999/xlink", "href", "logo_dvd.jpg");
+c.setAttribute("x", 0);
+c.setAttribute("y", 0);
+c.setAttribute("height", 75);
+c.setAttribute("width", 100);
+
+
 var bounce = function(){
 
-    x = x + x_v;
-    y = y + y_v;
 
-    if(x >= width - dvd_h || x <= 0){
-	x_v = x_v * -1;
-    } else if (y >= height - dvd_w || y <= 0){
-	y_v = y_v * -1;
-    }
+    var x = 0;
+    var y = 0;
+    var x_v = 2;
+    var y_v = 2;
+
+    var width = 500;
+    var height = 500;
+    var dvd_h = 75;
+    var dvd_w = 100;
     
-     var c = document.createElementNS(
-	 "http://www.w3.org/2000/svg", "image");
-    c.setAttribute("xlink", "logo_dvd.jpg");
-    c.setAttribute("x", x);
-    c.setAttribute("y", y);
-    c.setAttribute("height", 75);
-    c.setAttribute("width", 100);
+    var animateCode = function(){
+	
+	x = x + x_v;
+	y = y + y_v;
 
-    pic.appendChild(c);
+	if(x >= width - dvd_h || x <= 0){
+	    x_v = x_v * -1;
+	} else if (y >= height - dvd_w || y <= 0){
+	    y_v = y_v * -1;
+	}
 
-    requestId = window.requestAnimationFrame(bounce);
+	c.setAttribute("x", x);
+	c.setAttribute("y", y);
+	pic.appendChild(c);
+    }
+
+	intervalId = window.setInterval(animateCode, 16);
 
 };
 
