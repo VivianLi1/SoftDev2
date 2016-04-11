d6771ecdc78f7f3392bc75ce3fc62a9f1000f526
VivianLi1 <vivian2938@gmail.com>
2016-04-11 12:33:36 -0400

    stuff

--- HW09/decorator.py
+++ HW09/decorator.py
@@ -1,1 +1,27 @@
-placeholder
+def make_bold(fn):
+    return lambda : "<b>" + fn() + "</b>"
+
+def make_italic(fn):
+    return lambda : "<i>" + fn() + "</i>"
+
+@make_bold
+@make_italic
+def hello():
+    return "hello world"
+
+helloHTML = hello()
+
+print helloHTML
+
+def timer(f):
+    def inner(*arg):
+        t=time.time()
+        x=f(*arg)
+        print "execution time: " + str(time.time() - t)
+        return x
+    return timer
+        
+def randlist(fn , lower=-100, upper=100):
+    l = []
+    for i in range(n):
+        l.append(random.randrange(lower,upper)
